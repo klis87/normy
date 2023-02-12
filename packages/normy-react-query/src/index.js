ef5109232a6807cdbc7781ccf2bab675a6651bb6
klis87 <klisiczynski@gmail.com>
2023-02-12 18:08:50 +0100

    Refactor createNormalizedQueryClient and add optimistic updates

--- packages/normy-react-query/src/index.js
+++ packages/normy-react-query/src/index.js
@@ -7,45 +7,80 @@ export const createNormalizedQueryClient = (
 ) => {
   const normalizer = createNormalizer(normalizerConfig);
 
-  const queryCache = new QueryCache({
-    onSuccess: (data, query) => {
-      normalizer.onQuerySuccess(query.queryKey.join(','), data);
-    },
-  });
+  const queryCache = new QueryCache();
 
   queryCache.subscribe(event => {
     if (event.type === 'removed') {
       normalizer.onQueryRemoval(event.query.queryKey.join(','));
+    } else if (
+      event.type === 'updated' &&
+      event.action.type === 'success' &&
+      event.action.data
+    ) {
+      normalizer.onQuerySuccess(
+        event.query.queryKey.join(','),
+        event.action.data,
+      );
     }
   });
 
-  const queryClient = new QueryClient({
-    ...reactQueryConfig,
-    queryCache,
-    mutationCache: new MutationCache({
-      onSuccess: data => {
-        normalizer.onMutationSuccess(data, queriesToUpdate => {
+  const mutationCache = new MutationCache();
+
+  mutationCache.subscribe(event => {
+    if (
+      event.type === 'updated' &&
+      event.action.type === 'success' &&
+      event.action.data
+    ) {
+      normalizer.onMutationSuccess(event.action.data, queriesToUpdate => {
+        queriesToUpdate.forEach(query => {
+          queryClient.setQueryData(query.queryKey.split(','), () => query.data);
+        });
+      });
+    } else if (
+      event.type === 'updated' &&
+      event.action.type === 'loading' &&
+      event.mutation.state &&
+      event.mutation.state.context &&
+      event.mutation.state.context.optimisticData
+    ) {
+      normalizer.onMutationSuccess(
+        event.mutation.state.context.optimisticData,
+        queriesToUpdate => {
           queriesToUpdate.forEach(query => {
             queryClient.setQueryData(
               query.queryKey.split(','),
               () => query.data,
             );
           });
-        });
-      },
-    }),
+        },
+      );
+    } else if (
+      event.type === 'updated' &&
+      event.action.type === 'error' &&
+      event.mutation.state &&
+      event.mutation.state.context &&
+      event.mutation.state.context.rollbackData
+    ) {
+      normalizer.onMutationSuccess(
+        event.mutation.state.context.rollbackData,
+        queriesToUpdate => {
+          queriesToUpdate.forEach(query => {
+            queryClient.setQueryData(
+              query.queryKey.split(','),
+              () => query.data,
+            );
+          });
+        },
+      );
+    }
   });
 
-  const original = queryClient.setQueryData.bind(queryClient);
-
-  queryClient.setQueryData = (queryKey, data) => {
-    original(queryKey, data);
-
-    normalizer.onQuerySuccess(
-      queryKey.join(','),
-      queryClient.getQueryData(queryKey),
-    );
-  };
+  const queryClient = new QueryClient({
+    ...reactQueryConfig,
+    queryCache,
+    mutationCache,
+  });
 
   return queryClient;
 };
