a2a2beee5e0729ba621a9625f185d49797a68545
klis87 <klisiczynski@gmail.com>
2023-03-01 23:23:28 +0100

    Add removeQuery tests

--- packages/normy/src/create-normalizer.spec.ts
+++ packages/normy/src/create-normalizer.spec.ts
@@ -1,27 +1,12 @@
 import { createNormalizer } from './create-normalizer';
 
 describe('createNormalizer', () => {
-  it('sets initial query data', () => {
-    const normalizer = createNormalizer();
-    normalizer.setQuery('query', { id: '1', name: 'name' });
+  describe('setQuery', () => {
+    it('sets initial query data', () => {
+      const normalizer = createNormalizer();
+      normalizer.setQuery('query', { id: '1', name: 'name' });
 
-    expect(normalizer.getNormalizedData()).toEqual({
-      queries: {
-        query: {
-          data: '@@1',
-          dependencies: ['@@1'],
-          usedKeys: { '': ['id', 'name'] },
-        },
-      },
-      objects: { '@@1': { id: '1', name: 'name' } },
-      dependentQueries: { '@@1': ['query'] },
-    });
-  });
-
-  it('properly extends existing objects', () => {
-    const normalizer = createNormalizer(
-      {},
-      {
+      expect(normalizer.getNormalizedData()).toEqual({
         queries: {
           query: {
             data: '@@1',
@@ -31,98 +16,161 @@ describe('createNormalizer', () => {
         },
         objects: { '@@1': { id: '1', name: 'name' } },
         dependentQueries: { '@@1': ['query'] },
-      },
-    );
-    normalizer.setQuery('query2', {
-      id: '1',
-      name: 'name',
-      surname: 'surname',
+      });
     });
 
-    expect(normalizer.getNormalizedData()).toEqual({
-      queries: {
-        query: {
-          data: '@@1',
-          dependencies: ['@@1'],
-          usedKeys: { '': ['id', 'name'] },
-        },
-        query2: {
-          data: '@@1',
-          dependencies: ['@@1'],
-          usedKeys: { '': ['id', 'name', 'surname'] },
-        },
-      },
-      objects: { '@@1': { id: '1', name: 'name', surname: 'surname' } },
-      dependentQueries: { '@@1': ['query', 'query2'] },
-    });
-  });
-
-  it('works with nested objects', () => {
-    const normalizer = createNormalizer();
-    normalizer.setQuery('query', {
-      topLevel: {
-        id: '1',
-        name: 'name',
-        nested: { id: '2', name: 'name' },
-      },
-    });
-
-    expect(normalizer.getNormalizedData()).toEqual({
-      queries: {
-        query: {
-          data: { topLevel: '@@1' },
-          dependencies: ['@@1', '@@2'],
-          usedKeys: {
-            '.topLevel': ['id', 'name', 'nested'],
-            '.topLevel.nested': ['id', 'name'],
+    it('properly extends existing objects', () => {
+      const normalizer = createNormalizer(
+        {},
+        {
+          queries: {
+            query: {
+              data: '@@1',
+              dependencies: ['@@1'],
+              usedKeys: { '': ['id', 'name'] },
+            },
           },
+          objects: { '@@1': { id: '1', name: 'name' } },
+          dependentQueries: { '@@1': ['query'] },
         },
-      },
-      objects: {
-        '@@1': { id: '1', name: 'name', nested: '@@2' },
-        '@@2': { id: '2', name: 'name' },
-      },
-      dependentQueries: { '@@1': ['query'], '@@2': ['query'] },
-    });
-  });
-
-  it('works with arrays', () => {
-    const normalizer = createNormalizer();
-    normalizer.setQuery('query', [
-      {
+      );
+      normalizer.setQuery('query2', {
         id: '1',
         name: 'name',
-      },
-      {
-        id: '2',
-        name: 'name 2',
-      },
-    ]);
+        surname: 'surname',
+      });
 
-    expect(normalizer.getNormalizedData()).toEqual({
-      queries: {
-        query: {
-          data: ['@@1', '@@2'],
-          dependencies: ['@@1', '@@2'],
-          usedKeys: { '': ['id', 'name'] },
-        },
-      },
-      objects: {
-        '@@1': { id: '1', name: 'name' },
-        '@@2': { id: '2', name: 'name 2' },
-      },
-      dependentQueries: { '@@1': ['query'], '@@2': ['query'] },
-    });
-  });
-
-  it('properly extends arrays', () => {
-    const normalizer = createNormalizer(
-      {},
-      {
+      expect(normalizer.getNormalizedData()).toEqual({
         queries: {
           query: {
             data: '@@1',
-            dependencies: ['@@1', '@@2', '@@3'],
+            dependencies: ['@@1'],
+            usedKeys: { '': ['id', 'name'] },
+          },
+          query2: {
+            data: '@@1',
+            dependencies: ['@@1'],
+            usedKeys: { '': ['id', 'name', 'surname'] },
+          },
+        },
+        objects: { '@@1': { id: '1', name: 'name', surname: 'surname' } },
+        dependentQueries: { '@@1': ['query', 'query2'] },
+      });
+    });
+
+    it('works with nested objects', () => {
+      const normalizer = createNormalizer();
+      normalizer.setQuery('query', {
+        topLevel: {
+          id: '1',
+          name: 'name',
+          nested: { id: '2', name: 'name' },
+        },
+      });
+
+      expect(normalizer.getNormalizedData()).toEqual({
+        queries: {
+          query: {
+            data: { topLevel: '@@1' },
+            dependencies: ['@@1', '@@2'],
+            usedKeys: {
+              '.topLevel': ['id', 'name', 'nested'],
+              '.topLevel.nested': ['id', 'name'],
+            },
+          },
+        },
+        objects: {
+          '@@1': { id: '1', name: 'name', nested: '@@2' },
+          '@@2': { id: '2', name: 'name' },
+        },
+        dependentQueries: { '@@1': ['query'], '@@2': ['query'] },
+      });
+    });
+
+    it('works with arrays', () => {
+      const normalizer = createNormalizer();
+      normalizer.setQuery('query', [
+        {
+          id: '1',
+          name: 'name',
+        },
+        {
+          id: '2',
+          name: 'name 2',
+        },
+      ]);
+
+      expect(normalizer.getNormalizedData()).toEqual({
+        queries: {
+          query: {
+            data: ['@@1', '@@2'],
+            dependencies: ['@@1', '@@2'],
+            usedKeys: { '': ['id', 'name'] },
+          },
+        },
+        objects: {
+          '@@1': { id: '1', name: 'name' },
+          '@@2': { id: '2', name: 'name 2' },
+        },
+        dependentQueries: { '@@1': ['query'], '@@2': ['query'] },
+      });
+    });
+
+    it('properly extends arrays', () => {
+      const normalizer = createNormalizer(
+        {},
+        {
+          queries: {
+            query: {
+              data: '@@1',
+              dependencies: ['@@1', '@@2', '@@3'],
+              usedKeys: {
+                '': ['id', 'list'],
+                '.list': ['id', 'name'],
+              },
+            },
+          },
+          objects: {
+            '@@1': { id: '1', list: ['@@2', '@@3'] },
+            '@@2': {
+              id: '2',
+              name: 'name 2',
+            },
+            '@@3': {
+              id: '3',
+              name: 'name 3',
+            },
+          },
+          dependentQueries: {
+            '@@1': ['query'],
+            '@@2': ['query'],
+            '@@3': ['query'],
+          },
+        },
+      );
+      normalizer.setQuery('query', {
+        id: '1',
+        list: [
+          {
+            id: '2',
+            name: 'name 2',
+          },
+          {
+            id: '3',
+            name: 'name 3',
+          },
+          {
+            id: '4',
+            name: 'name 4',
+          },
+        ],
+      });
+
+      expect(normalizer.getNormalizedData()).toEqual({
+        queries: {
+          query: {
+            data: '@@1',
+            dependencies: ['@@1', '@@2', '@@3', '@@4'],
             usedKeys: {
               '': ['id', 'list'],
               '.list': ['id', 'name'],
@@ -130,7 +178,7 @@ describe('createNormalizer', () => {
           },
         },
         objects: {
-          '@@1': { id: '1', list: ['@@2', '@@3'] },
+          '@@1': { id: '1', list: ['@@2', '@@3', '@@4'] },
           '@@2': {
             id: '2',
             name: 'name 2',
@@ -139,131 +187,168 @@ describe('createNormalizer', () => {
             id: '3',
             name: 'name 3',
           },
+          '@@4': {
+            id: '4',
+            name: 'name 4',
+          },
         },
         dependentQueries: {
           '@@1': ['query'],
           '@@2': ['query'],
           '@@3': ['query'],
+          '@@4': ['query'],
         },
-      },
-    );
-    normalizer.setQuery('query', {
-      id: '1',
-      list: [
-        {
-          id: '2',
-          name: 'name 2',
-        },
-        {
-          id: '3',
-          name: 'name 3',
-        },
-        {
-          id: '4',
-          name: 'name 4',
-        },
-      ],
+      });
     });
 
-    expect(normalizer.getNormalizedData()).toEqual({
-      queries: {
-        query: {
-          data: '@@1',
-          dependencies: ['@@1', '@@2', '@@3', '@@4'],
-          usedKeys: {
-            '': ['id', 'list'],
-            '.list': ['id', 'name'],
+    it('works without objects to normalize', () => {
+      const normalizer = createNormalizer();
+      normalizer.setQuery('query', { name: 'name' });
+
+      expect(normalizer.getNormalizedData()).toEqual({
+        queries: {
+          query: {
+            data: { name: 'name' },
+            dependencies: [],
+            usedKeys: {},
           },
         },
-      },
-      objects: {
-        '@@1': { id: '1', list: ['@@2', '@@3', '@@4'] },
-        '@@2': {
-          id: '2',
-          name: 'name 2',
+        objects: {},
+        dependentQueries: {},
+      });
+    });
+
+    it('allows to override normalisation key', () => {
+      const normalizer = createNormalizer({
+        getNormalisationObjectKey: obj => obj._id as string,
+      });
+      normalizer.setQuery('query', {
+        _id: '1',
+        name: 'name',
+        notNormalizableObj: { id: '2' },
+      });
+
+      expect(normalizer.getNormalizedData()).toEqual({
+        queries: {
+          query: {
+            data: '@@1',
+            dependencies: ['@@1'],
+            usedKeys: { '': ['_id', 'name', 'notNormalizableObj'] },
+          },
         },
-        '@@3': {
-          id: '3',
-          name: 'name 3',
+        objects: {
+          '@@1': { _id: '1', name: 'name', notNormalizableObj: { id: '2' } },
         },
-        '@@4': {
-          id: '4',
-          name: 'name 4',
+        dependentQueries: { '@@1': ['query'] },
+      });
+    });
+
+    it('allows to disable normalisation per object', () => {
+      const normalizer = createNormalizer({
+        getNormalisationObjectKey: obj => obj._id as string,
+      });
+      normalizer.setQuery('query', {
+        _id: '1',
+        name: 'name',
+        notNormalizableObj: { id: '2' },
+      });
+
+      expect(normalizer.getNormalizedData()).toEqual({
+        queries: {
+          query: {
+            data: '@@1',
+            dependencies: ['@@1'],
+            usedKeys: { '': ['_id', 'name', 'notNormalizableObj'] },
+          },
         },
-      },
-      dependentQueries: {
-        '@@1': ['query'],
-        '@@2': ['query'],
-        '@@3': ['query'],
-        '@@4': ['query'],
-      },
+        objects: {
+          '@@1': { _id: '1', name: 'name', notNormalizableObj: { id: '2' } },
+        },
+        dependentQueries: { '@@1': ['query'] },
+      });
     });
   });
 
-  it('works without objects to normalize', () => {
-    const normalizer = createNormalizer();
-    normalizer.setQuery('query', { name: 'name' });
-
-    expect(normalizer.getNormalizedData()).toEqual({
-      queries: {
-        query: {
-          data: { name: 'name' },
-          dependencies: [],
-          usedKeys: {},
+  describe('removeQuery', () => {
+    it('does nothing if a query to remove does not exist', () => {
+      const state = {
+        queries: {
+          query: {
+            data: '@@1',
+            dependencies: ['@@1'],
+            usedKeys: { '': ['id', 'name'] },
+          },
         },
-      },
-      objects: {},
-      dependentQueries: {},
-    });
-  });
+        objects: { '@@1': { id: '1', name: 'name' } },
+        dependentQueries: { '@@1': ['query'] },
+      };
 
-  it('allows to override normalisation key', () => {
-    const normalizer = createNormalizer({
-      getNormalisationObjectKey: obj => obj._id as string,
-    });
-    normalizer.setQuery('query', {
-      _id: '1',
-      name: 'name',
-      notNormalizableObj: { id: '2' },
+      const normalizer = createNormalizer({}, state);
+      normalizer.removeQuery('query2');
+
+      expect(normalizer.getNormalizedData()).toEqual(state);
     });
 
-    expect(normalizer.getNormalizedData()).toEqual({
-      queries: {
-        query: {
-          data: '@@1',
-          dependencies: ['@@1'],
-          usedKeys: { '': ['_id', 'name', 'notNormalizableObj'] },
+    it('removes a query if it exists', () => {
+      const normalizer = createNormalizer(
+        {},
+        {
+          queries: {
+            query: {
+              data: '@@1',
+              dependencies: ['@@1'],
+              usedKeys: { '': ['id', 'name'] },
+            },
+          },
+          objects: { '@@1': { id: '1', name: 'name' } },
+          dependentQueries: { '@@1': ['query'] },
         },
-      },
-      objects: {
-        '@@1': { _id: '1', name: 'name', notNormalizableObj: { id: '2' } },
-      },
-      dependentQueries: { '@@1': ['query'] },
-    });
-  });
+      );
+      normalizer.removeQuery('query');
 
-  it('allows to disable normalisation per object', () => {
-    const normalizer = createNormalizer({
-      getNormalisationObjectKey: obj => obj._id as string,
-    });
-    normalizer.setQuery('query', {
-      _id: '1',
-      name: 'name',
-      notNormalizableObj: { id: '2' },
+      expect(normalizer.getNormalizedData()).toEqual({
+        queries: {},
+        objects: {},
+        dependentQueries: {},
+      });
     });
 
-    expect(normalizer.getNormalizedData()).toEqual({
-      queries: {
-        query: {
-          data: '@@1',
-          dependencies: ['@@1'],
-          usedKeys: { '': ['_id', 'name', 'notNormalizableObj'] },
+    it('removes a query if it exists without affecting other queries', () => {
+      const normalizer = createNormalizer(
+        {},
+        {
+          queries: {
+            query: {
+              data: '@@1',
+              dependencies: ['@@1'],
+              usedKeys: { '': ['id', 'name'] },
+            },
+            query2: {
+              data: '@@2',
+              dependencies: ['@@2'],
+              usedKeys: { '': ['id', 'name'] },
+            },
+          },
+          objects: {
+            '@@1': { id: '1', name: 'name' },
+            '@@2': { id: '2', name: 'name' },
+          },
+          dependentQueries: { '@@1': ['query'], '@@2': ['query2'] },
         },
-      },
-      objects: {
-        '@@1': { _id: '1', name: 'name', notNormalizableObj: { id: '2' } },
-      },
-      dependentQueries: { '@@1': ['query'] },
+      );
+      normalizer.removeQuery('query2');
+
+      expect(normalizer.getNormalizedData()).toEqual({
+        queries: {
+          query: {
+            data: '@@1',
+            dependencies: ['@@1'],
+            usedKeys: { '': ['id', 'name'] },
+          },
+        },
+        objects: { '@@1': { id: '1', name: 'name' } },
+        dependentQueries: { '@@1': ['query'] },
+      });
     });
   });
 });
